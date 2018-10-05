87c4356b25cb4eb9912f018a3ed84e4bc93b97b3
Mason Do <pdo9141@gmail.com>
2018-10-05 01:17:16 -0700

    Initial commit

---
+++ ConsoleApp/RedisStore.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp {
+    public class RedisStore {
+        private static readonly Lazy<ConnectionMultiplexer> LazyConnection;
+
+        static RedisStore() {
+            var configurationOptions = new ConfigurationOptions {
+                EndPoints = { ConfigurationManager.AppSettings["redis.connection"] }
+            };
+
+            LazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(configurationOptions));
+        }
+
+        public static ConnectionMultiplexer Connection => LazyConnection.Value;
+
+        public static IDatabase RedisCache => Connection.GetDatabase();
+    }
+}
